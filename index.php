c2ebcfcb6c877d17ba852c8e641bd164c86a3fa9
hkfmz <hyperpro200497@gmail.com>
2021-02-19 03:36:55 +0100

    second commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Prodcastor</title>
+</head>
+<body>
+    <header>
+        <h1 class="title">PRODCASTOR</h1>
+        <nav>
+            <ul class="list">
+                <li>Home</li>
+                <li>Prodcast</li>
+                <li>Compte</li>
+                <li>FAQ</li>
+            </ul>
+        </nav>
+        <form action="" method="post" class="form">
+                <label for="search"></label>
+                <input type="search" name="query" id="query" autocomplete="off">
+                <button type="submit" class="btn">Rechercher</button>
+        </form>
+    </header>
+<br>
+<br>
+<br>
+    <main>
+           <div class="menu">
+              <button style="background-color: #34495e; color: white">Client <hr>
+                 <br>                 
+                 <br>                 
+                 <br>
+            </button>
+              <button style="background-color: #34495e; color: white">Fournisseur <hr>
+                 <br>                 
+                 <br>                 
+                 <br>
+            </button>
+              <button style="background-color: #34495e; color: white">Ventes <hr>
+                 <br>                 
+                 <br>                 
+                 <br>
+            </button>
+              <button style="background-color: #34495e; color: white">Achat <hr>
+                 <br>                 
+                 <br>                 
+                 <br>
+            </button>
+              <button style="background-color: #34495e; color: white">Article <hr>
+                 <br>                 
+                 <br>                 
+                 <br>
+            </button>
+           </div>
+    </main>
+
+</body>
+</html>
